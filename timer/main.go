a02b9869acb5360f0d2f90d6272ab4da181af2a3
Stuart Scott <stuart.murray.scott@gmail.com>
2020-04-07 12:33:28 -0700

    Use Data Bindings in Timer

---
+++ timer/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/binding"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Timer")
+
+	// Create bindings
+	bindDuration := &binding.Float64Binding{}
+	bindElapsed := &binding.Float64Binding{}
+	bindElapsedString := &binding.StringBinding{}
+
+	// Create widgets
+	elapsedProgressBar := widget.NewProgressBar()
+	elapsedProgressBar.BindValue(bindElapsed)
+	elapsedProgressBar.BindMax(bindDuration)
+	elapsedLabel := widget.NewLabel("")
+	elapsedLabel.BindText(bindElapsedString)
+	durationSlider := widget.NewSlider(0, 100)
+	durationSlider.BindValue(bindDuration)
+	resetButton := widget.NewButton("Reset", func() {
+		bindElapsed.Set(0.0)
+	})
+
+	// Configure pipeline
+	bindElapsed.AddListener(func(f float64) {
+		bindElapsedString.Set(fmt.Sprintf("%.1fs", f))
+	})
+
+	// Create timer to increment elapsed time
+	done := make(chan bool)
+	defer func() {
+		done <- true
+	}()
+	timer := time.NewTicker(time.Second)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-timer.C:
+				d := bindDuration.Get()
+				e := bindElapsed.Get()
+				if e < d {
+					bindElapsed.Set(e + 1)
+				}
+			}
+		}
+	}()
+
+	w.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+		widget.NewLabel("Elapsed Time:"), elapsedProgressBar,
+		layout.NewSpacer(), elapsedLabel,
+		widget.NewLabel("Duration:"), durationSlider,
+		layout.NewSpacer(), resetButton))
+	w.ShowAndRun()
+}
