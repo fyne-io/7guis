6d6925708e9c721795c161641d840edb0ff5f714
Stuart Scott <stuart.murray.scott@gmail.com>
2020-04-09 14:25:07 -0700

    Uses bind chaining, simplifies type conversion, and adds comments

--- temperature-converter/main.go
+++ temperature-converter/main.go
@@ -11,23 +11,16 @@ import (
 	"fyne.io/fyne/widget"
 )
 
-func isNumeric(s string) bool {
-	_, err := strconv.ParseFloat(s, 64)
-	return err == nil
-}
-
 func main() {
 	a := app.New()
 	w := a.NewWindow("Temperature Converter")
 
 	// Create binding
-	bindingKelvin := NewKelvinBinding()
+	temperature := NewTemperatureBinding()
 
 	// Create widgets
-	inputC := widget.NewEntry()
-	inputC.BindText(bindingKelvin.Celsius)
-	inputF := widget.NewEntry()
-	inputF.BindText(bindingKelvin.Fahrenheit)
+	inputC := widget.NewEntry().BindText(temperature.Celsius)
+	inputF := widget.NewEntry().BindText(temperature.Fahrenheit)
 
 	w.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(4),
 		inputC, widget.NewLabel("Celsius ="), inputF, widget.NewLabel("Fahrenheit")))
@@ -35,30 +28,20 @@ func main() {
 	w.ShowAndRun()
 }
 
-type KelvinBinding struct {
+// TemperatureBinding implements a data binding for temperature in Kelvin.
+type TemperatureBinding struct {
 	binding.Float64Binding
 	Celsius, Fahrenheit *binding.StringBinding
 }
 
-func NewKelvinBinding() *KelvinBinding {
-	kb := &KelvinBinding{
-		Celsius:    &binding.StringBinding{},
-		Fahrenheit: &binding.StringBinding{},
+// NewTemperatureBinding creates and returns a new TemperatureBinding.
+func NewTemperatureBinding() *TemperatureBinding {
+	kb := &TemperatureBinding{
+		Celsius:    binding.NewStringBinding(""),
+		Fahrenheit: binding.NewStringBinding(""),
 	}
-	kb.Celsius.AddListener(func(text string) {
-		f, err := strconv.ParseFloat(text, 64)
-		if err != nil {
-			return
-		}
-		kb.SetCelsius(f)
-	})
-	kb.Fahrenheit.AddListener(func(text string) {
-		f, err := strconv.ParseFloat(text, 64)
-		if err != nil {
-			return
-		}
-		kb.SetFahrenheit(f)
-	})
+	kb.Celsius.AddListener(parseFloat64To(kb.SetCelsius))
+	kb.Fahrenheit.AddListener(parseFloat64To(kb.SetFahrenheit))
 	kb.AddListener(func(float64) {
 		kb.Celsius.Set(fmt.Sprintf("%.2f", kb.GetCelsius()))
 		kb.Fahrenheit.Set(fmt.Sprintf("%.2f", kb.GetFahrenheit()))
@@ -66,18 +49,31 @@ func NewKelvinBinding() *KelvinBinding {
 	return kb
 }
 
-func (b *KelvinBinding) GetCelsius() float64 {
+// GetCelsius returns the temperature converted to Celsius.
+func (b *TemperatureBinding) GetCelsius() float64 {
 	return b.Get() - 273.15
 }
 
-func (b *KelvinBinding) GetFahrenheit() float64 {
+// GetFahrenheit returns the temperature converted to Fahrenheit.
+func (b *TemperatureBinding) GetFahrenheit() float64 {
 	return (b.Get()-273.15)*9.0/5.0 + 32
 }
 
-func (b *KelvinBinding) SetCelsius(c float64) {
+// SetCelsius updates the temperature to the given reading in Celsius.
+func (b *TemperatureBinding) SetCelsius(c float64) {
 	b.Set(c + 273.15)
 }
 
-func (b *KelvinBinding) SetFahrenheit(f float64) {
+// SetFahrenheit updates the temperature to the given reading in Fahrenheit.
+func (b *TemperatureBinding) SetFahrenheit(f float64) {
 	b.Set((f-32)*5.0/9.0 + 273.15)
+}
+
+func parseFloat64To(c func(float64)) func(string) {
+	return func(s string) {
+		f, err := strconv.ParseFloat(s, 64)
+		if err == nil {
+			c(f)
+		}
+	}
 }
