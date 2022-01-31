ff9dc7d759cfac0130859b087dead231c4ded063
Andy Williams <andy@andy.xyz>
2022-01-30 17:41:51 -0800

    Add create/update/delete functions

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 )
 
 func main() {
+	selected := -1
 	a := app.New()
 	w := a.NewWindow("CRUD")
 
+	name := widget.NewEntry()
+	surname := widget.NewEntry()
 	list := widget.NewList(func() int {
 		return len(people)
 	}, func() fyne.CanvasObject {
@@ -18,17 +21,47 @@ func main() {
 	}, func(id widget.ListItemID, o fyne.CanvasObject) {
 		o.(*widget.Label).SetText(people[id].String())
 	})
+	list.OnSelected = func(id widget.ListItemID) {
+		selected = id
+		name.SetText(people[id].name)
+		surname.SetText(people[id].surname)
+	}
 
 	form := widget.NewForm(
-		widget.NewFormItem("Name", widget.NewEntry()),
-		widget.NewFormItem("Surname", widget.NewEntry()))
+		widget.NewFormItem("Name", name),
+		widget.NewFormItem("Surname", surname))
 
 	top := container.NewGridWithColumns(2,
 		widget.NewForm(widget.NewFormItem("Filter prefix:", widget.NewEntry())))
 	bottom := container.NewHBox(
-		widget.NewButton("Create", func() {}),
-		widget.NewButton("Update", func() {}),
-		widget.NewButton("Delete", func() {}))
+		widget.NewButton("Create", func() {
+			p := &person{name: name.Text, surname: surname.Text}
+			people = append(people, p)
+			list.Refresh()
+		}),
+		widget.NewButton("Update", func() {
+			if selected < 0 || selected >= len(people) {
+				return
+			}
+
+			people[selected].name = name.Text
+			people[selected].surname = surname.Text
+			list.Refresh()
+		}),
+		widget.NewButton("Delete", func() {
+			if selected < 0 || selected >= len(people) || len(people) == 0 {
+				return
+			}
+
+			if selected == 0 {
+				people = people[1:]
+			} else if selected == len(people) - 1 {
+				people = people[:len(people)-1]
+			} else {
+				people = append(people[:selected], people[selected+1:]...)
+			}
+			list.Refresh()
+		}))
 
 	grid := container.NewGridWithColumns(2, list, form)
 	w.SetContent(
