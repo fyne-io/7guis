8100679003678148ee0395a2c7d1865dc733c0d2
Andy Williams <andy@andy.xyz>
2022-01-30 18:30:51 -0800

    Add support for filtering
    Disable mutation buttons when not selected

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+var filtered = noFilter()
 
 func main() {
 	selected := -1
@@ -15,11 +19,11 @@ func main() {
 	name := widget.NewEntry()
 	surname := widget.NewEntry()
 	list := widget.NewList(func() int {
-		return len(people)
+		return len(filtered)
 	}, func() fyne.CanvasObject {
 		return widget.NewLabel("")
 	}, func(id widget.ListItemID, o fyne.CanvasObject) {
-		o.(*widget.Label).SetText(people[id].String())
+		o.(*widget.Label).SetText(people[filtered[id]].String())
 	})
 
 	update := widget.NewButton("Update", func() {
@@ -32,7 +36,8 @@ func main() {
 		list.Refresh()
 	})
 	update.Disable()
-	delete := widget.NewButton("Delete", func() {
+	var delete *widget.Button
+	delete = widget.NewButton("Delete", func() {
 			if selected < 0 || selected >= len(people) || len(people) == 0 {
 				return
 			}
@@ -44,14 +49,18 @@ func main() {
 			} else {
 				people = append(people[:selected], people[selected+1:]...)
 			}
+			filtered = noFilter()
+			list.UnselectAll()
 			list.Refresh()
+			update.Disable()
+			delete.Disable()
 		})
 	delete.Disable()
 
 	list.OnSelected = func(id widget.ListItemID) {
-		selected = id
-		name.SetText(people[id].name)
-		surname.SetText(people[id].surname)
+		selected = filtered[id]
+		name.SetText(people[selected].name)
+		surname.SetText(people[selected].surname)
 
 		update.Enable()
 		delete.Enable()
@@ -65,12 +74,34 @@ func main() {
 		widget.NewFormItem("Name", name),
 		widget.NewFormItem("Surname", surname))
 
+	filter := widget.NewEntry()
+	filter.OnChanged = func(prefix string) {
+		list.UnselectAll()
+		update.Disable()
+		delete.Disable()
+		if prefix == "" {
+			filtered = noFilter()
+			list.Refresh()
+			return
+		}
+
+		prefix = strings.ToLower(prefix)
+		f := []int {}
+		for i, p := range people {
+			if strings.Index(strings.ToLower(p.surname), prefix) == 0 {
+				f = append(f, i)
+			}
+		}
+		filtered = f
+		list.Refresh()
+	}
 	top := container.NewGridWithColumns(2,
-		widget.NewForm(widget.NewFormItem("Filter prefix:", widget.NewEntry())))
+		widget.NewForm(widget.NewFormItem("Filter prefix:", filter)))
 	bottom := container.NewHBox(
 		widget.NewButton("Create", func() {
 			p := &person{name: name.Text, surname: surname.Text}
 			people = append(people, p)
+			filtered = noFilter()
 			list.Refresh()
 			list.Select(len(people)-1)
 		}),
@@ -80,4 +111,12 @@ func main() {
 	w.SetContent(
 		container.NewBorder(top, bottom, nil, nil, grid))
 	w.ShowAndRun()
+}
+
+func noFilter() []int {
+	all := make([]int, len(people))
+	for i := range people {
+		all[i] = i
+	}
+	return all
 }
